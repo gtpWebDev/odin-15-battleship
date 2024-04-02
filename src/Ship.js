0fb86f96e45d24c44f3bf4a6dc8df8e77077e8a7
gtpWebDev <33732289+gtpWebDev@users.noreply.github.com>
2024-04-02 16:31:29 +0100

    Improve createShip factory to be more robust

--- src/Ship.js
+++ src/Ship.js
@@ -1,7 +1,18 @@
-const createShip = (len = 0) => {
+const createShip = (len) => {
+  const noLengthMsg = 'ship length must be a number';
+  const incorrectLenMsg = 'ship length must be min 1, max 5';
+
+  if (typeof len !== 'number') {
+    throw new Error(noLengthMsg);
+  } else if (len <= 0 || len > 5) {
+    throw new Error(incorrectLenMsg);
+  }
+
   let length = len;
   let hitsTaken = 0;
   let sunk = false;
+
+  const getHitsTaken = () => hitsTaken;
 
   const hit = () => {
     hitsTaken++;
@@ -10,7 +21,7 @@ const createShip = (len = 0) => {
 
   const isSunk = () => sunk;
 
-  return { hit, isSunk };
+  return { getHitsTaken, hit, isSunk };
 };
 
 export { createShip as default };
--- src/Ship.test.js
+++ src/Ship.test.js
@@ -1,27 +1,49 @@
-import { test, expect, describe } from '@jest/globals';
+import { test, expect, describe, beforeAll } from '@jest/globals';
 
 import createShip from './Ship.js';
 
-/*
-Tests:
-  Test the ship methods first
-  Then at the end, when sure of the ship structure, test createShip
-*/
+test('Created ship with length not a number throws an error', () => {
+  expect(() => createShip()).toThrow('ship length must be a number');
+  expect(() => createShip(true)).toThrow('ship length must be a number');
+  expect(() => createShip('')).toThrow('ship length must be a number');
+  expect(() => createShip(null)).toThrow('ship length must be a number');
+  expect(() => createShip(undefined)).toThrow('ship length must be a number');
+});
 
-describe('ship.isSunk', () => {
-  const ship1 = createShip(3);
-  const ship2 = createShip(1);
-  test('createShip - isSunk', () => {
-    ship1.hit();
-    expect(ship1.isSunk()).toEqual(false);
-    ship1.hit();
-    expect(ship1.isSunk()).toEqual(false);
-    ship1.hit();
-    expect(ship1.isSunk()).toEqual(true);
+test('Created ship with length outside accepted range throws an error', () => {
+  expect(() => createShip(0)).toThrow('ship length must be min 1, max 5');
+  expect(() => createShip(6)).toThrow('ship length must be min 1, max 5');
+});
+
+describe('Created ship', () => {
+  const ship = createShip(3);
+  test('starts with hits taken zero', () => {
+    expect(ship.getHitsTaken()).toEqual(0);
   });
-  test('createShip - isSunk', () => {
-    expect(ship2.isSunk()).toEqual(false);
-    ship2.hit();
-    expect(ship2.isSunk()).toEqual(true);
+  test('starts not sunk', () => {
+    expect(ship.isSunk()).toEqual(false);
+  });
+});
+
+describe('Created ship of size 2 gets hit', () => {
+  const ship = createShip(2);
+  ship.hit();
+  test('has hitsTaken of 1', () => {
+    expect(ship.getHitsTaken()).toEqual(1);
+  });
+  test('but is not sunk', () => {
+    expect(ship.isSunk()).toEqual(false);
+  });
+});
+
+describe('Created ship of size 2 gets twice', () => {
+  const ship = createShip(2);
+  ship.hit();
+  ship.hit();
+  test('has hitsTaken of 2', () => {
+    expect(ship.getHitsTaken()).toEqual(2);
+  });
+  test('and is sunk', () => {
+    expect(ship.isSunk()).toEqual(true);
   });
 });
